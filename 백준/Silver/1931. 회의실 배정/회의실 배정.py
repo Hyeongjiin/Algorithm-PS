3ecd5344821342bcc961d7bcfa9c951f169c3619
Park HyeongJin <lipfolnp@gmail.com>
2023-11-27 21:40:57 +0900

    [Silver I] Title: 회의실 배정, Time: 408 ms, Memory: 126416 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,30 +1,20 @@
-from sys import stdin
-def solve():
-    n=int(stdin.readline().rstrip())
-    dic={}
-    trash=0
-    ltrash={}
-    for i in range(n):
-        a,b=map(int,stdin.readline().split())
-        if a not in dic and b!=a: dic[a]=b
-        elif b==a:
-            trash+=1
-            if b not in ltrash: ltrash[b]=0
-        elif dic[a]>b and b!=a: dic[a]=b 
-    for e in ltrash:
-        if e not in dic:
-            dic[e]=e
-            trash-=1
-    arr=list(dic)
-    arr.sort()
-    tmp=dic[arr[0]]
-    cnt=1
-    for i in range(1,len(arr)):
-        if tmp>dic[arr[i]]:
-            tmp=dic[arr[i]]
-        else:
-            if arr[i]>=tmp:
-                cnt+=1
-                tmp=dic[arr[i]]
-    print(cnt+trash)
-solve()
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+data = []
+end = 0
+for i in range(N):
+    time = input().split()
+    if int(time[1]) > end:
+        end = int(time[1])
+    data.append((int(time[0]), int(time[1])))
+data = sorted(data, key = lambda x:(x[1], x[0]))
+current = data[0][0]
+cnt = 0
+for i in range(N):
+    if current <= data[i][0]:
+        cnt += 1
+        current = data[i][1]
+print(cnt)       
