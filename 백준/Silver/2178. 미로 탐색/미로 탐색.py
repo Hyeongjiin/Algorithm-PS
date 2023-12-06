34249fcf090de4676c09f6be2a2685ce5fe4d35f
Park HyeongJin <lipfolnp@gmail.com>
2023-12-06 17:04:55 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 114084 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,28 @@
-import sys
 from collections import deque
+import sys
 
-def bfs(place, x, y):
-  q = deque([(x, y)])
-  while q:
-    position = q.popleft()
-    x = position[0]
-    y = position[1]
-    for move in moves:
-      nx = x + move[0]
-      ny = y + move[1]
-      if nx < 0 or ny < 0 or nx > N - 1 or ny > M - 1:
-        continue
-      if place[nx][ny] == 0:
-        continue
-      if place[nx][ny] == 1:
-        place[nx][ny] = place[x][y] + 1
-        q.append((nx, ny))
+def bfs(y, x, cnt):
+    queue = deque()
+    queue.append((y, x, cnt))
+    while queue:
+        y, x, cnt = queue.popleft()
+        if y < 0 or x < 0 or y >= N or x >= M:
+            continue
+        if y == N - 1 and x == M - 1:
+            cnt += 1
+            return cnt
+        if graph[y][x] == 1:
+            cnt += 1
+            graph[y][x] = -1
+            queue.append((y + 1, x, cnt))
+            queue.append((y - 1, x, cnt))
+            queue.append((y, x + 1, cnt))
+            queue.append((y, x - 1, cnt))
 
 N, M = map(int, input().split())
-place = []
+graph = []
 for i in range(N):
-  place.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
 
-moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-bfs(place, 0, 0)
-print(place[N - 1][M - 1])
+result = bfs(0, 0, 0)
+print(result)
