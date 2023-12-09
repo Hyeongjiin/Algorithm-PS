82c90516ee83610574d4dfc441a8b568296817d1
Park HyeongJin <lipfolnp@gmail.com>
2023-12-09 15:46:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 604 ms, Memory: 68892 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(graph, visited, R):
+  visited[R] = 1
+  result.append(R)
+  for i in graph[R]:
+    if visited[i] == 0:
+      dfs(graph, visited, i)
+  
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+  start, end = map(int, input().split())
+  graph[start].append(end)
+  graph[end].append(start)
+
+for i in range(1, N + 1):
+  graph[i].sort()
+
+visited = [0] * (N + 1)
+result = []
+dfs(graph, visited, R)
+order = [0] * (N + 1)
+for i in range(len(result)):
+  order[result[i]] = i + 1
+
+for i in range(1, N + 1):
+  print(order[i])
