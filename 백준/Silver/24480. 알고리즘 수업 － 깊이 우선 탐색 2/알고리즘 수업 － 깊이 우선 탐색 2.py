042a57099f7ee490640aae5b31c53963a67d7157
Park HyeongJin <lipfolnp@gmail.com>
2023-12-09 15:49:45 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 604 ms, Memory: 71180 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -4,10 +4,11 @@ sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 def dfs(graph, visited, R):
-  visited[R] = 1
-  result.append(R)
+  global count
+  visited[R] = count
   for i in graph[R]:
     if visited[i] == 0:
+      count += 1
       dfs(graph, visited, i)
   
 input = sys.stdin.readline
@@ -23,11 +24,7 @@ for i in range(1, N + 1):
   graph[i].sort(reverse=True)
 
 visited = [0] * (N + 1)
-result = []
+count = 1
 dfs(graph, visited, R)
-order = [0] * (N + 1)
-for i in range(len(result)):
-  order[result[i]] = i + 1
-
 for i in range(1, N + 1):
-  print(order[i])
+  print(visited[i])
