a6861ea0c95c8e6c8a850689299586cb5dadb5f9
Park HyeongJin <lipfolnp@gmail.com>
2023-12-21 23:43:17 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stairs = []
+for i in range(N):
+  stairs.append(int(input()))
+
+dp = [0] * N
+dp[0] = stairs[0]
+if N > 1:
+  dp[1] = stairs[0] + stairs[1]
+if N > 2:
+  dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+
+if N > 4:
+  for i in range(3, N):
+    dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i])
+
+print(dp[N - 1])
