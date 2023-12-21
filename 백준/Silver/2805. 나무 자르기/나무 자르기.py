dae17adf46b29fe6131640420969a39a277babcb
Park HyeongJin <lipfolnp@gmail.com>
2023-12-21 19:18:08 +0900

    [Silver II] Title: 나무 자르기, Time: 484 ms, Memory: 258028 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,25 +1,23 @@
 import sys
 
-def b_search(data, M, start, end):
-    result = 0
-    while start <= end:
-        count = 0
-        mid = (start + end) // 2
-        for i in data:
-            if i > mid:
-                count += i - mid
-        if count < M:
-            end = mid - 1
-        else:
-            start = mid + 1
-            result = mid
-    return result
-      
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-data = list(map(int, input().split()))
+woods = list(map(int, input().split()))
+
 start = 0
-end = 2 * max(data)
-height = b_search(data, M, start, end)
-print(height)
+end = max(woods)
+
+result = 0
+while start <= end:
+  total = 0
+  mid = (start + end) // 2
+  for i in woods:
+    if i > mid:
+      total += i - mid
+  if total < M:
+    end = mid - 1
+  else:
+    result = mid
+    start = mid + 1
+print(result)
