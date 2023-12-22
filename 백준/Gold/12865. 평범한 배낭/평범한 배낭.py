a1ca704f801dfda9c2ae01a2b3594231ad9e91fe
Park HyeongJin <lipfolnp@gmail.com>
2023-12-22 23:50:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 308 ms, Memory: 194188 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,17 @@
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
-items = [(0, 0)]
+data = [(0, 0)]
 for i in range(N):
-  items.append(tuple(map(int, input().split())))
-
+    W, V = map(int, input().split())
+    data.append((W, V))
 dp = [[0] * (K + 1) for _ in range(N + 1)]
-
 for i in range(1, N + 1):
-  weight = items[i][0]
-  value = items[i][1]
-  for j in range(1, K + 1):
-    if weight > j:
-      dp[i][j] = dp[i - 1][j]
-    else:
-      dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
-
-print(dp[N][K])
+    weight = data[i][0]
+    value = data[i][1]
+    for j in range(1, K + 1):
+        if j < weight:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
+print(max(dp[N]))
