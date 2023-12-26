80c79f7756d41ca19a3f5c3d64e302f01d757036
Park HyeongJin <lipfolnp@gmail.com>
2023-12-26 15:18:46 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,12 @@
 import sys
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
 
-for _ in range(n):
-  s = list(sys.stdin.readline().strip())
-  a=[]
-  for i in s:
-    a.append(i)
-    if i == ')':
-      if len(a)==1:
-        break
-      a.pop()
-      a.pop()
-  if len(a)!=0:
-    print('NO')
+N = int(input())
+for i in range(N):
+  S = input().strip()
+  for i in range(30):
+    S = S.replace("()", "")
+  if S == "":
+    print("YES")
   else:
-    print('YES')
+    print("NO")
