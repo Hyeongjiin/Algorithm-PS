cd3860e66687e426ec803a99cb9432baa0d5fd72
Park HyeongJin <lipfolnp@gmail.com>
2024-01-17 00:02:28 +0900

    [Silver II] Title: N과 M (12), Time: 56 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def sequence(data, M):
+
+  def backtrack(curr, start):
+    if len(curr) == M:
+      ans.append(curr[:])
+      return
+
+    for i in range(start, len(data)):
+      curr.append(data[i])
+      backtrack(curr, i)
+      curr.pop()
+
+  ans = []
+  curr = []
+  backtrack(curr, 0)
+  return ans
+
+
+N, M = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+result = sequence(data, M)
+result = list(set(map(tuple, result)))
+result.sort()
+for i in result:
+  print(*i)
