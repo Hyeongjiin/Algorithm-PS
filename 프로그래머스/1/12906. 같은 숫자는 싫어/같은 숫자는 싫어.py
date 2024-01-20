91d94e03a1be7f384f43ca7655a7ec6bd68d5a00
Park HyeongJin <lipfolnp@gmail.com>
2024-01-20 21:29:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 95.24 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)):
+        if not answer:
+            answer.append(arr[i])
+        else:
+            if answer[-1] != arr[i]:
+                answer.append(arr[i])
+    return answer
