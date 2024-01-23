e2bf2d17ca3061772607b4276811dfb747970c0a
Park HyeongJin <lipfolnp@gmail.com>
2024-01-23 15:59:55 +0900

    [level 2] Title: 주식가격, Time: 22.76 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,8 +1,23 @@
 def solution(prices):
     answer = [0] * len(prices)
-    for i in range(len(prices)):
-        for j in range(i + 1, len(prices)):
-            answer[i] += 1
-            if prices[i] > prices[j]:
+    stack = [(prices[0], 0)]
+    turn = 1
+    while stack:
+        if prices[turn] >= stack[-1][0]:
+            stack.append((prices[turn], turn))
+            turn += 1
+            if turn == len(prices):
                 break
+        else:
+            down = stack.pop()
+            answer[down[1]] = turn - down[1]
+            if not stack:
+                stack.append((prices[turn], turn))
+                turn += 1
+                if turn == len(prices):
+                    break
+    if stack:
+        last = stack.pop()
+        for i in stack:
+            answer[i[1]] = last[1] - i[1]
     return answer
