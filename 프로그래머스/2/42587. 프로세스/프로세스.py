1f83620c188cc28c3ca54f1c39f73ae04a59aa7a
Park HyeongJin <lipfolnp@gmail.com>
2024-01-23 11:45:19 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    q = deque()
+    for i in range(len(priorities)):
+        q.append((priorities[i], i))
+    
+    maxi = max(q, key = lambda x : x[0])[0]
+
+    count = 0
+    while q:
+        cur = q.popleft()
+        if cur[0] < maxi:
+            q.append(cur)
+        else:
+            count += 1
+            if cur[1] == location:
+                answer = count
+                break
+            maxi = max(q, key = lambda x : x[0])[0]
+    
+
+    return answer
