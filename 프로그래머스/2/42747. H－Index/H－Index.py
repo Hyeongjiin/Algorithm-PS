eb9bed23b57a8e4a929758ebe30a71d5f373b720
Park HyeongJin <lipfolnp@gmail.com>
2024-01-25 15:44:54 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,7 @@
 def solution(citations):
-    start = 0
-    end = max(citations)
-    max_citation = 0
-    while start <= end:
-        mid = (start + end) // 2
-        count = 0
-        for citation in citations:
-            if citation >= mid:
-                count += 1
-        if count >= mid:
-            start = mid + 1
-            if mid > max_citation:
-                max_citation = mid
-        else:
-            end = mid - 1
-    answer = max_citation
-    return answer
+    citations.sort()
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l - i:
+            return l - i
+    return 0
