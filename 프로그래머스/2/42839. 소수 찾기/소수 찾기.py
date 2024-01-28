97f53b78faa8dd3b709422fcfec259230f787829
Park HyeongJin <lipfolnp@gmail.com>
2024-01-28 19:08:11 +0900

    [level 2] Title: 소수 찾기, Time: 18.82 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,14 +1,36 @@
 def solution(numbers):
+    '''   
+    전부 다 일일이 소수 계산
     papers = list(numbers)
     result = list(set(sequence(papers)))
     answer = 0
     print(result)
     for i in result:
         if prime(i):
+            answer += 1 
+    '''
+    # 에라토스테네스의 체 사용해 소수 판별하기
+    papers = list(numbers)
+    result = list(set(sequence(papers)))
+    result.sort()
+    answer = 0
+    max_num = max(result)
+    table = [True] * (max_num + 1)
+    for i in range(2, max_num + 1):
+        if table[i] == True:
+            j = 2
+            while i * j <= max_num:
+                table[i * j] = False
+                j += 1                
+    for i in result:
+        if i < 2:
+            continue
+        if table[i] == True:
             answer += 1
 
     return answer
 
+# 숫자 조합 만들기 - visited를 통해 사용한 숫자 재사용 하지 않기
 def sequence(data):
     def backtrack(curr, length):
         if len(curr) == length:
@@ -28,6 +50,7 @@ def sequence(data):
         backtrack(curr, i)
     return result
 
+# 일일이 소수 판별하기 
 def prime(num):
     answer = True
     if num < 2:
