18e2d02c59bcda97be963372c85b41b7855f02be
Park HyeongJin <lipfolnp@gmail.com>
2024-01-28 18:28:53 +0900

    [level 1] Title: 모의고사, Time: 3.31 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,18 @@
-import math
-
 def solution(answers):
-    answer = []
-    length = len(answers)
-    f = [1, 2, 3, 4, 5] * math.ceil(length / 5)
-    s = [2, 1, 2, 3, 2, 4, 2, 5] * math.ceil(length / 8)
-    t = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * math.ceil(length / 10)
+    result = []
+    f = [1, 2, 3, 4, 5] 
+    s = [2, 1, 2, 3, 2, 4, 2, 5]
+    t = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     counts = [0, 0, 0]
-
-    for i in range(len(answers)):
-        if f[i] == answers[i]:
+    for idx, answer in enumerate(answers):
+        if answer == f[idx % len(f)]:
             counts[0] += 1
-        if s[i] == answers[i]:
+        if answer == s[idx % len(s)]:
             counts[1] += 1
-        if t[i] == answers[i]:
+        if answer == t[idx % len(t)]:
             counts[2] += 1
     max_count = max(counts)
     for i in range(3):
         if counts[i] == max_count:
-            answer.append(i + 1)
-    return answer
+            result.append(i + 1)
+    return result
