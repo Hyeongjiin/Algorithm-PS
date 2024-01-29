1294a20a593c64d6db0d5e528dfcc6467be7b909
Park HyeongJin <lipfolnp@gmail.com>
2024-01-29 22:57:52 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,28 @@
+from collections import deque
+import heapq
+
+def solution(jobs):
+    total_reqs = len(jobs)
+    jobs.sort()
+    jobs = deque(jobs)
+    q = []
+    cur_time = jobs[0][0]
+    req_time = 0
+    while jobs:
+        while jobs and jobs[0][0] <= cur_time:
+            req = jobs.popleft()
+            heapq.heappush(q, (req[1], req[0]))
+        if q:
+            print(q)
+            cur_job = heapq.heappop(q)
+            req_time += cur_job[0] + cur_time - cur_job[1]
+            cur_time += cur_job[0]
+        else:
+            cur_time = jobs[0][0]
+            
+    while q:
+        cur_job = heapq.heappop(q)
+        req_time += cur_job[0] + cur_time - cur_job[1]
+        cur_time += cur_job[0]
+    answer = req_time // total_reqs
+    return answer
