95dd69fa654b22e26f6742baba85e5ebbac15673
Park HyeongJin <lipfolnp@gmail.com>
2024-01-30 00:29:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,29 +3,30 @@ import heapq
 def solution(operations):
     max_q = []
     min_q = []
-    count = 0
-    for operation in operations:
+    used = [0] * len(operations)
+    for idx, operation in enumerate(operations):
+        print(operation, idx)
         operation = operation.split(' ')
         operator = operation[0]
         num = int(operation[1])
         if operator == 'I':
-            heapq.heappush(min_q, num)
-            heapq.heappush(max_q, -num)
-            count += 1
+            heapq.heappush(min_q, (num, idx))
+            heapq.heappush(max_q, (-num, idx))
         elif operator == 'D':
             if num == 1 and max_q:
-                heapq.heappop(max_q)
-                count -= 1
+                out = heapq.heappop(max_q)
+                used[out[1]] = 1
             elif num == -1 and min_q:
-                heapq.heappop(min_q)
-                count -= 1
-        if count == 0:
-            max_q = []
-            min_q = []
+                out = heapq.heappop(min_q)
+                used[out[1]] = 1
+        while max_q and used[max_q[0][1]] == 1:
+            heapq.heappop(max_q)
+        while min_q and used[min_q[0][1]] == 1:
+            heapq.heappop(min_q)             
     answer = []
     if not max_q:
         answer = [0, 0]
     else:
-        answer.append(-heapq.heappop(max_q))
-        answer.append(heapq.heappop(min_q))
+        answer.append(-heapq.heappop(max_q)[0])
+        answer.append(heapq.heappop(min_q)[0])
     return answer
