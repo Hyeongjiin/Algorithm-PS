9cc12fce21a0fa1fc21dd5d8ad674b3f1809b40f
Park HyeongJin <lipfolnp@gmail.com>
2024-02-02 22:28:15 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,5 +1,15 @@
 def solution(n, times):
-    print(n)
-    print(times)
-    answer = 0
+    start = 1
+    end = max(times) * n
+    answer = int(10e12)
+    while start <= end:
+        mid = (start + end) // 2
+        count = 0
+        for time in times:
+            count += mid // time
+        if count < n:
+            start = mid + 1
+        elif count >= n:
+            end = mid - 1
+            answer = min(answer, mid)
     return answer
