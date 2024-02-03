16bea17f81230fc71cd668c8d7c99d0c2ec1e76d
Park HyeongJin <lipfolnp@gmail.com>
2024-02-03 14:36:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,31 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
-    N = len(maps)
-    M = len(maps[0])
-    x, y = 0, 0
-    q = deque([(x, y)])
+    width = len(maps[0])
+    height = len(maps)
     moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    while q:
-        current = q.popleft()
-        x = current[0]
-        y = current[1]
-        if x == N - 1 and y == M - 1:
-            answer = maps[N -1][M - 1]
-        for move in moves:
-            nx = x + move[0]
-            ny = y + move[1]
-            if nx < 0 or ny < 0 or nx > N - 1 or ny > M - 1:
-                continue
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                q.append((nx, ny))
-    if answer == 0:
-        answer = -1
+    visited = [[False] * width for _ in range(height)]
+    answer = bfs(maps, visited, width, height, moves, 0, 0)
     return answer
+
+
+def bfs(maps, visited, width, height, moves, x, y):
+    q = deque()
+    count = 1
+    q.append((x, y, count))
+    visited[x][y] = True
+    while q:
+        cur_x, cur_y, cur_count = q.popleft()
+        if cur_x == height - 1 and cur_y == width - 1:
+            return cur_count
+        for move in moves:
+            nx = cur_x + move[0]
+            ny = cur_y + move[1]
+            if nx < 0 or nx > height - 1 or ny < 0 or ny > width - 1:
+                continue
+            if maps[nx][ny] == 0:
+                continue
+            if visited[nx][ny] == False:
+                visited[nx][ny] = True
+                q.append((nx, ny, cur_count + 1))
+    return -1
