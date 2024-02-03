55946f5b3b55b195810533e9d5297414e4d06e4e
Park HyeongJin <lipfolnp@gmail.com>
2024-02-03 13:44:41 +0900

    [level 2] Title: 타겟 넘버, Time: 2.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,13 @@
+'''
+BFS
 from collections import deque
 
 def solution(numbers, target):
     answer = 0
     q = deque()
     length = len(numbers) - 1
+    global answer
+    answer = 0
     total = 0
     q.append((total + numbers[0], 0))
     q.append((total - numbers[0], 0))
@@ -17,3 +21,25 @@ def solution(numbers, target):
         q.append((cur_total - numbers[cur_idx + 1], cur_idx + 1))
 
     return answer
+'''
+
+#DFS
+def solution(numbers, target):
+    global answer
+    answer = 0
+    length = len(numbers) - 1
+    dfs(0, -1, numbers, target, length)
+
+    return answer
+
+def dfs(cur_total, cur_idx, numbers, target, length):
+    global answer
+    if cur_idx == length:
+        if cur_total == target:
+            answer += 1
+        return 
+    dfs(cur_total + numbers[cur_idx + 1], cur_idx + 1, numbers, target, length)
+    dfs(cur_total - numbers[cur_idx + 1], cur_idx + 1, numbers, target, length)
+
+    
+    
