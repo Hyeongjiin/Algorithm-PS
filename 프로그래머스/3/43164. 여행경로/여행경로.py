0fd232e7891bee1bf5cf1668818f292f94cdc935
Park HyeongJin <lipfolnp@gmail.com>
2024-02-04 22:38:44 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 ways = {}
 visited =  {}
 answer = []
@@ -38,6 +36,37 @@ def dfs(start, count, path, length):
             dfs(ways[start][i], count + 1, path + [ways[start][i]], length)
             visited[start][i] = 0
             
+    
+"""
+WOW - DFS
+from collections import deque
+
+def solution(tickets):
+    ways = {}
+    for ticket in tickets:
+        if ticket[0] not in ways:
+            ways[ticket[0]] = [ticket[1]]
+        else:
+            ways[ticket[0]].append(ticket[1])
+        if ticket[1] not in ways:
+            ways[ticket[1]] = []
+    for val in ways.values():
+        val.sort()
+    
+    answer = []
+    dfs(ways, answer, "ICN")
+    answer = answer[::-1]
+    return answer
+
+def dfs(ways, result, start):
+    while ways[start]:
+        dfs(ways, result, ways[start].pop(0))
+    
+    if not ways[start]:
+        result.append(start)
+        return 
+    
+"""
     
 """
 WOW - DFS
