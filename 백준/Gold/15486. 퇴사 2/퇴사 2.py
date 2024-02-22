2fd1f2cc8b79f25c864c4f8c5e2d87a90ae09c2e
Park HyeongJin <lipfolnp@gmail.com>
2024-02-22 16:24:55 +0900

    [Gold V] Title: 퇴사 2, Time: 784 ms, Memory: 122732 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -3,19 +3,14 @@ import sys
 sys = sys.stdin.readline
 
 N = int(input())
-data = [(0, 0, 0)]
 dp = [0] * (N + 2)
-
-for i in range(1, N + 1):
-  T, P = map(int, input().split())
-  data.append((i, T, P))
-
 answer = 0
 
 for i in range(1, N + 1):
+  T, P = map(int, input().split())
   dp[i] = max(dp[i], dp[i - 1])
-  if i + data[i][1] <= N + 1:
-    dp[i + data[i][1]] = max(dp[i + data[i][1]], dp[i] + data[i][2])
+  if i + T <= N + 1:
+    dp[i + T] = max(dp[i + T], dp[i] + P)
   answer = max(answer, dp[i])
 answer = max(answer, dp[N + 1])
 print(answer)
