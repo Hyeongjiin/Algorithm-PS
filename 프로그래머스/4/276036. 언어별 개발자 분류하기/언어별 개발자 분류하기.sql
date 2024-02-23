8cab1957616aba53de4051657f725b7be274b33d
Park HyeongJin <lipfolnp@gmail.com>
2024-02-23 18:56:53 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,28 +1,40 @@
-SELECT A.GRADE, A.ID, A.EMAIL
+# SELECT SUM(CODE) 
+# FROM SKILLCODES
+# WHERE CATEGORTY = 'Front End'
+
+# SELECT SUM(CODE) 
+# FROM SKILLCODES
+# WHERE NAME = 'Python'
+
+# SELECT SUM(CODE) 
+# FROM SKILLCODES
+# WHERE NAME = 'C#'
+SELECT *
 FROM (
-    SELECT *, CASE
+    SELECT CASE
         WHEN SKILL_CODE & (
-            SELECT SUM(CODE)
+            SELECT SUM(CODE) 
             FROM SKILLCODES
-            WHERE CATEGORY = 'Front End'   
+            WHERE CATEGORY = 'Front End'
         ) > 0 AND SKILL_CODE & (
-            SELECT CODE
+            SELECT CODE 
             FROM SKILLCODES
-            WHERE NAME = 'Python'   
-        ) THEN 'A'
+            WHERE NAME = 'Python'
+        ) > 0 THEN 'A'
         WHEN SKILL_CODE & (
-            SELECT CODE
+            SELECT CODE 
             FROM SKILLCODES
             WHERE NAME = 'C#'    
         ) > 0 THEN 'B'
         WHEN SKILL_CODE & (
-            SELECT SUM(CODE)
+            SELECT SUM(CODE) 
             FROM SKILLCODES
-            WHERE CATEGORY = 'Front End'   
+            WHERE CATEGORY = 'Front End'
         ) > 0 THEN 'C'
-        ELSE NULL 
-        END AS GRADE
+        ELSE NULL
+        END AS GRADE, ID, EMAIL
     FROM DEVELOPERS
 ) AS A
 WHERE GRADE IS NOT NULL
 ORDER BY GRADE, ID
+
