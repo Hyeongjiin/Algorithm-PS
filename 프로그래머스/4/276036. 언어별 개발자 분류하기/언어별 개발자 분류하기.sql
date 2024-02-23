e531e1a6d4f611336cbf54447db11c6150ff7d9b
Park HyeongJin <lipfolnp@gmail.com>
2024-02-23 13:05:08 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,43 @@
+SELECT A.GRADE, A.ID, A.EMAIL
+FROM (
+    SELECT *, CASE
+        WHEN SKILL_CODE & (
+            SELECT SUM(CODE)
+            FROM SKILLCODES
+            WHERE CATEGORY = 'Front End'   
+        ) > 0 AND SKILL_CODE & (
+            SELECT CODE
+            FROM SKILLCODES
+            WHERE NAME = 'Python'   
+        ) THEN 'A'
+        WHEN SKILL_CODE & (
+            SELECT CODE
+            FROM SKILLCODES
+            WHERE NAME = 'C#'    
+        ) > 0 THEN 'B'
+        WHEN SKILL_CODE & (
+            SELECT SUM(CODE)
+            FROM SKILLCODES
+            WHERE CATEGORY = 'Front End'   
+        ) > 0 THEN 'C'
+        ELSE NULL 
+        END AS GRADE
+    FROM DEVELOPERS
+) AS A
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID
+
+
+
+
+# SELECT SUM(CODE)
+# FROM SKILLCODES
+# WHERE CATEGORY = 'Front End'
+
+# SELECT CODE
+# FROM SKILLCODES
+# WHERE NAME = 'Python'
+
+# SELECT CODE
+# FROM SKILLCODES
+# WHERE NAME = 'C#'
